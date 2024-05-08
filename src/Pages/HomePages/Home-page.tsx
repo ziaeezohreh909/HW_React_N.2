46fef44b937f038d38954104e088398d4cec5b05
“ziaeezohreh909” <ziaeezohreh909@gmail.com>
2024-05-08 16:32:24 +0330

    creat layout&product components

---
+++ src/Pages/HomePages/Home-page.tsx
@@ -0,0 +1,12 @@
+import Product from "../../Products/Product";
+import Layout from "../../components/Layout/Layout";
+
+export default function Homepage() {
+  return (
+    <div>
+      <Layout>
+        <Product />
+      </Layout>
+    </div>
+  );
+}
---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,17 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import { ReactNode } from "react";
+
+type Props = {
+  children?: ReactNode;
+};
+
+export default function Layout({ children }: Props) {
+  return (
+    <div>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+}
